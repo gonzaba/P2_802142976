77ac1b426515c88558dce92bd220c19e803d2b86
Barbara P. Gonzalez-Rivera <barbara.gonzalez1@upr.edu>
2018-04-17 18:14:16 -0400

    Made Customer Class

--- p2_40354020_172/src/classes/Customer.java
+++ p2_40354020_172/src/classes/Customer.java
@@ -10,7 +10,98 @@ package classes;
  */
 
 public class Customer {
+			private int id;
+			private int arrivalTime;
+			private int serviceTime;
+			private int remainingTime;
+			private int departureTime;
+			
+			
+		//Setters	
+			
+		/**
+ 		* 
+ 		* @param i set the id of the costumer to i
+ 		*/
+			
+		public void setId(int i) {
+			id = i;
+		}
+		
+		/**
+		* 
+		* @param t set the arrival time of the customer to t
+		* 
+		*/
+		
+		public void setArrivalTime(int t) {
+			arrivalTime = t;
+		}
+		
+		/**
+		 * 
+		 * @param s set the service time it takes customer to be serviced
+		 */
+		
+		public void setServiceTime(int s) {
+			serviceTime = s;
+		}
+		
+		/**
+		 * 
+		 * @param d set the depatureTime of the customer
+		 */
+		public void setDepartureTime(int d) {
+			departureTime = d;
+		}
+		/**
+		 * 
+		 * @param r set the amount of time it takes for order to be completed
+		 */
+		public void setRemainingTime(int r) {
+			remainingTime = r;
+		}
+		
+		
+		//Getters
+		
+		public int getId() {
+			return id;
+		}
+		
+		public int getArrivalTime() {
+			return arrivalTime;
+		}
+		
+		public int getServiceTime() {
+			return serviceTime;
+		}
+		
+		public int getDepartureTime() {
+			return departureTime;
+		}
+		
+		public int getRemainingTime() {
+			return remainingTime;
+		}
+		
+		
+		
+		/**
+		 * decreases the remaining time by one number
+		 */
+		public void decreaseRemainingTime() {
+			remainingTime--;
+		}
+		
+		
+		
+		
+		
+		
+	}
 
-}
+
+
 
 
