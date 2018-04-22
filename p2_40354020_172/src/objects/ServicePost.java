df3aa828820cfbbd3b31cf56aae344cf5a6f62e5
Barbara P. Gonzalez-Rivera <barbara.gonzalez1@upr.edu>
2018-04-21 20:32:58 -0400

    commented service post

--- p2_40354020_172/src/objects/ServicePost.java
+++ p2_40354020_172/src/objects/ServicePost.java
@@ -8,6 +8,14 @@ import classes.ArrayQueue;
  * ICOM4035 - 030
  */
 
+/**
+ * 
+ * This class will handle what is technically a the cashier in the simulation.
+ * It can verify if its available, who is it serving and the line it currently has 
+ * for that service post.
+ *
+ */
+
 public class ServicePost {
 
 		//boolean that describe if the server is available
@@ -50,14 +58,17 @@ public class ServicePost {
 			
 		//Getters
 		
+		//Getter to that says is the server is available to take a new customer or not
 		public boolean isAvailable() {
 			return isAvailable;
 		}
 		
+		//getter to get the customer that the server is currently serving
 		public Customer getCustomer() {
 			return person;
 		}
 		
+		//getter to ge the list the current server has 
 		public ArrayQueue<Customer> getPersonalWaitingLine(){
 			return personalWaitingLine;
 		}
